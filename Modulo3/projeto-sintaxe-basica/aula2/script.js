9e3483d66a629733766992e9fc9c99c7fccef1bf
peguima <pedroveigaguima@gmail.com>
2022-06-02 13:42:00 -0300

    início do desenvolvimento do módulo 3

---
+++ Modulo3/projeto-sintaxe-basica/aula2/script.js
@@ -0,0 +1,100 @@
+// tipos primitivos em JavaScript:
+
+//boolean
+/* var vOuF = false;
+console.log(typeof(vOuF)); 
+// o comando console.log é o que direciona o navegador a mostrar o dado no console do navegador, não diretamente para o usuário;
+// o comando typeof possibilita mostrar que tipo primitivo aquela variável está fazendo menção
+// se não houvesse o comando typeof, o console iria mostrar o que estivesse discriminado nos parêntesis
+
+// number
+ var numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
+
+// string
+var nome = 'pedro';
+console.log(typeof(nome)); */
+
+//como declarar
+/* var variavel = 'pedro';
+console.log(variavel);
+
+let variavel2 = 'pedro';
+console.log(variavel2);
+
+const constante = 'pedro';
+console.log(constante); */
+
+// Escopo
+/* var escopoGlobal = 'global'
+console.log(escopoGlobal);
+
+function escopoLocal() {
+    let escopoLocalInterno = 'local';
+    console.log(escopoLocalInterno);
+}
+
+escopoLocal();
+
+// atribuição
+var atribuicao = 'pedro';
+
+// comparação
+var comparacao = '0'== 0;
+console.log(comparacao);
+
+// comparação identica
+var comparacaoIdentica = '0'=== 0;
+console.log(comparacaoIdentica);
+
+// adicao
+var adicao = 1 + 1;
+console.log(adicao);
+
+// subtracao
+var subtracao = 2 - 1;
+console.log(subtracao);
+
+// multiplicacao
+var multiplicacao = 2 * 3;
+console.log(multiplicacao);
+
+// divisao real
+var divisaoReal = 6 / 3;
+console.log(divisaoReal);
+
+// divisão inteira
+var divisaoInteira = 5 % 2;
+console.log(divisaoInteira);
+
+// potenciacao
+var potenciacao = 2 ** 10;
+console.log(potenciacao);
+
+// maior que
+var maiorQue = 5 > 2;
+console.log(maiorQue);
+
+// menor que
+var menorQue = 5 < 2;
+console.log(menorQue);
+
+// maior ou igual a
+var maiorOuIgual = 5 >= 2;
+console.log(maiorOuIgual);
+
+// menor ou igual
+var menorOuIgual = 5 <= 2;
+console.log(menorOuIgual);
+
+// é
+var e = true && false;
+console.log(e);
+
+// ou
+var ou = true || false;
+console.log(ou);
+
+// não
+var nao = !true;
+console.log(nao); */
---
+++ Modulo3/projeto-sintaxe-basica/aula3/script.js
@@ -0,0 +1,56 @@
+// o que são vetores ou arrays
+
+// como declarar um array
+/* let array = ['string', 1, true]
+console.log(array);
+
+// pode guardar vários tipos de dados
+let array = ['string', 1, true, ['array1'], ['array2'], ['array3'], ['array4']];
+console.log(array);
+
+// forEach
+array.forEach(function(item, index){console.log(item, index)});
+
+// push
+array.push('novo item');
+console.log(array);
+
+// pop
+array.pop();
+console.log(array);
+
+// shift
+array.shift();
+console.log(array);
+
+// unshift
+array.unshift('novo item no início');
+console.log(array);
+
+//indexOf
+console.log(array.indexOf(true));
+
+// splice
+array.splice(0, 3);
+console.log(array);
+
+// slice
+let novoArray = array.slice(0, 3);
+console.log(novoArray);
+
+let object = {string: 'string', number: 1, boolean: true, array: ["array", objectIterno: {objetointerno: 'objeto interno'}]};
+console.log(object);
+
+// para acessar uma propriedade:
+console.log(object.boolean);
+
+// desestruração do objeto
+var string = object.string
+console.log(string);
+
+var arrayInterno = object.array;
+console.log(arrayInterno);
+
+// outra forma de fazer desestruturação
+var { string, boolean, objectInterno} = object;
+console.log(string, boolean, objectInterno); */
